59c38570facae04f9e018aa54377f78d38ba55b3
Oleg Baranov <xssnick8@gmail.com>
2024-02-20 19:52:39 +0400

    Fixed metrics registration

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type Metrics struct {
@@ -16,31 +17,31 @@ var Global *Metrics
 
 func InitMetrics(namespace, subsystem string) {
 	Global = &Metrics{
-		ActiveADNLConnections: prometheus.NewGauge(prometheus.GaugeOpts{
+		ActiveADNLConnections: promauto.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
 			Name:      "adnl_connections",
 			Help:      "Active ADNL TCP connections with clients",
 		}),
-		Requests: prometheus.NewCounterVec(prometheus.CounterOpts{
+		Requests: promauto.NewCounterVec(prometheus.CounterOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
 			Name:      "adnl_requests",
 			Help:      "Raw ADNL requests",
 		}, []string{"key_name", "request_type"}),
-		LSErrors: prometheus.NewCounterVec(prometheus.CounterOpts{
+		LSErrors: promauto.NewCounterVec(prometheus.CounterOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
 			Name:      "ls_errors",
 			Help:      "LSError responses count",
 		}, []string{"key_name", "request_type", "code"}),
-		Queries: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Queries: promauto.NewHistogramVec(prometheus.HistogramOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
 			Name:      "ls_queries",
 			Help:      "LS Requests to proxy statistics",
 		}, []string{"key_name", "request_type", "hit_type"}),
-		BackendQueries: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		BackendQueries: promauto.NewHistogramVec(prometheus.HistogramOpts{
 			Namespace: namespace,
 			Subsystem: subsystem,
 			Name:      "backend_queries",
